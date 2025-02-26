8459d6e54b505230728377d249953e13a8c1366a
Reginaldo Junior <rsajr12@gmail.com>
2025-02-26 14:48:41 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+//menu responsivo
+document.addEventListener('DOMContentLoaded', function () {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('active');
+    });
+});
